d0b98c89c5596e69c7cf249f166f100d64659bfc
candyburst <townshipiamgroot@gmail.com>
2025-02-02 07:05:26 +0530

    Update game.js

--- src/services/game.js
+++ src/services/game.js
@@ -48,7 +48,7 @@ class GameService {
   }
 
   async claimGame(user, lang, gameId, eligibleDogs) {
-    const randomPoints = user?.database?.randomPoints || [175, 225];
+    const randomPoints = user?.database?.randomPoints || [170, 205];
     const multiplierPoint = user?.database?.multiplierPoint || 1;
     let points = generatorHelper.randomInt(randomPoints[0], randomPoints[1]);
     let dogs = 0;
